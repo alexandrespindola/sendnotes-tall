513f3d9a3d57ae08dd7f79870e6333210f63fcc2
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-12-16 20:10:21 +0100

    Adiciona comando para enviar notas agendadas e job para envio de e-mails; implementa placeholder de carregamento na visualização de notas.

---
+++ app/Jobs/SendEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use App\Models\Note;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmail implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Note $note)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $noteUrl = config('app.url') . ':8000/notes/' . $this->note->id;
+
+        $emailContent = "Hello! You have a new note to read. Click here to view it: {$noteUrl}";
+
+        Mail::raw($emailContent, function ($message) {
+            $message->from('alexandre@titansdev.es', 'Sendnotes')
+                ->to($this->note->recipient)
+                ->subject('You have a new note from ' . $this->note->user->name);
+        });
+    }
+}
