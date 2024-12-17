e9604d53e4fdf11d79e415fbd8a6f7eee28fa61d
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-12-17 10:32:42 +0100

    Adiciona arquivos Kernel.php para gerenciamento de agendamentos e middleware na aplicação

---
+++ app/Console/Kernel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    // ...existing code...
+
+    protected function schedule(Schedule $schedule)
+    {
+        // ...existing code...
+    }
+
+    protected function commands()
+    {
+        // ...existing code...
+    }
+}
---
+++ app/Http/Kernel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    // ...existing code...
+
+    protected $middleware = [
+        // ...existing code...
+    ];
+
+    protected $middlewareGroups = [
+        // ...existing code...
+    ];
+
+    protected $routeMiddleware = [
+        // ...existing code...
+    ];
+}
